531ea9af44cff82f813f896d3f5d3579c4f7d917
MAHMOODUR RAHMAN <mahmoodr@student.ubc.ca>
2022-03-03 19:41:50 -0800

    tbshoot_3

--- src/app.py
+++ src/app.py
@@ -1,135 +1,84 @@
-from dash import Dash, html, dcc, Input, Output
+import time
+
+import dash
 import dash_bootstrap_components as dbc
-import altair as alt
-import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+from dash import Input, Output, dcc, html
 
-# disable Altair limits
-qwl_df = pd.read_csv("./data/bei_vita_qwl_assessment.csv")
-
-app = Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP, "https://codepen.io/chriddyp/pen/bWLwgP.css"]
-)
-server = app.server
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container(
     [
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        html.Br(),
-                        html.H2("Bei Vita"),
-                        html.P(
-                            dcc.Markdown(
-                                """
-                                Visualization to represent how scores for quality of
-                                work life are distributed
-                                """
-                            )
-                        )
-                    ],
-                    width=3
-                ),
-                dbc.Col(
-                    [
-                        html.H1("Quality of Work Life"),
-                        html.H2("Client Name")
-                    ],
-                    width=6
-                ),
-                dbc.Col(
-                    [
-                    ],
-                    width=3
-                )
-            ],
-            align="center"
+        dcc.Store(id="store"),
+        html.H1("Dynamically rendered tab content"),
+        html.Hr(),
+        dbc.Button(
+            "Regenerate graphs",
+            color="primary",
+            id="button",
+            className="mb-3",
         ),
-        html.Br(),
-
-        dbc.Row(
+        dbc.Tabs(
             [
-                
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.Iframe(
-                                    id="lineplot",
-                                    style={"border-width": "0", "width": "100%", "height": "450px"}
-                                ),
-                                dcc.Dropdown(
-                                    id='xcol-lineplot-widget',
-                                    value='Total score',
-                                    options=[{'label': col, 'value': col} for col in ["Total score"]]
-                                )
-                            ]
-                        )
-                    ],
-                    width=9
-                ),
-                dbc.Col(
-                    [
-                   
-                    ],
-                    width=3
-                ),
+                dbc.Tab(label="Scatter", tab_id="scatter"),
+                dbc.Tab(label="Histograms", tab_id="histogram"),
             ],
+            id="tabs",
+            active_tab="scatter",
         ),
-        html.Br(),
-
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        
-                    ],
-                ),
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.Iframe(
-                                    id="vertical_barplot",
-                                    # value="Total score",
-                                    style={"border-width": "0", "width": "100%", "height": "450px"}
-                                ),
-                                dcc.Dropdown(
-                                    id='xcol-vbarplot-widget',
-                                    value='Total score',
-                                    options=[{'label': col, 'value': col} for col in ["Total score"]]
-                                )
-                            ]
-                        )
-                    ]
-                ),
-            ],
-        )
+        html.Div(id="tab-content", className="p-4"),
     ]
 )
 
+
 @app.callback(
-    Output("lineplot", "srcDoc"),
-    Output("vertical_barplot", "srcDoc"),
-    Input('xcol-lineplot-widget', 'value'),
-    Input('xcol-vbarplot-widget', 'value')
+    Output("tab-content", "children"),
+    [Input("tabs", "active_tab"), Input("store", "data")],
 )
-def vertical_barplot(xcol_lineplot, xcol_vbarplot):
-    lineplot = alt.Chart(qwl_df).mark_line().encode(
-        x=xcol_lineplot,
-        y="count()",
-    ).properties(width=600).interactive()
+def render_tab_content(active_tab, data):
+    """
+    This callback takes the 'active_tab' property as input, as well as the
+    stored graphs, and renders the tab content depending on what the value of
+    'active_tab' is.
+    """
+    if active_tab and data is not None:
+        if active_tab == "scatter":
+            return dcc.Graph(figure=data["scatter"])
+        elif active_tab == "histogram":
+            return dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=data["hist_1"]), width=6),
+                    dbc.Col(dcc.Graph(figure=data["hist_2"]), width=6),
+                ]
+            )
+    return "No tab selected"
 
-    vertical_barplot = alt.Chart(qwl_df).mark_bar().encode(
-        x=alt.X(
-            xcol_vbarplot,
-            bin=alt.Bin(maxbins=5)
-        ),
-        y="count()",
+
+@app.callback(Output("store", "data"), [Input("button", "n_clicks")])
+def generate_graphs(n):
+    """
+    This callback generates three simple graphs from random data.
+    """
+    if not n:
+        # generate empty graphs when app loads
+        return {k: go.Figure(data=[]) for k in ["scatter", "hist_1", "hist_2"]}
+
+    # simulate expensive graph generation process
+    time.sleep(2)
+
+    # generate 100 multivariate normal samples
+    data = np.random.multivariate_normal([0, 0], [[1, 0.5], [0.5, 1]], 100)
+
+    scatter = go.Figure(
+        data=[go.Scatter(x=data[:, 0], y=data[:, 1], mode="markers")]
     )
+    hist_1 = go.Figure(data=[go.Histogram(x=data[:, 0])])
+    hist_2 = go.Figure(data=[go.Histogram(x=data[:, 1])])
 
-    return lineplot.to_html(), vertical_barplot.to_html(), 
+    # save figures in a dictionary for sending to the dcc.Store
+    return {"scatter": scatter, "hist_1": hist_1, "hist_2": hist_2}
+
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True, port=8888)
