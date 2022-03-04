b8f6362680e046cdbe8140259257c56ed12c31bc
MAHMOODUR RAHMAN <mahmoodr@student.ubc.ca>
2022-03-03 20:30:52 -0800

    trblshoot_12

--- src/app.py
+++ src/app.py
@@ -1,135 +1,26 @@
-from dash import Dash, html, dcc, Input, Output
-import dash_bootstrap_components as dbc
-import altair as alt
-import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+import os
 
-# disable Altair limits
-qwl_df = pd.read_csv("./data/bei_vita_qwl_assessment.csv")
 
-app = Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP, "https://codepen.io/chriddyp/pen/bWLwgP.css"]
-)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
 
-app.layout = dbc.Container(
-    [
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        html.Br(),
-                        html.H2("Bei Vita"),
-                        html.P(
-                            dcc.Markdown(
-                                """
-                                Visualization to represent how scores for quality of
-                                work life are distributed
-                                """
-                            )
-                        )
-                    ],
-                    width=3
-                ),
-                dbc.Col(
-                    [
-                        html.H1("Quality of Work Life"),
-                        html.H2("Client Name")
-                    ],
-                    width=6
-                ),
-                dbc.Col(
-                    [
-                    ],
-                    width=3
-                )
-            ],
-            align="center"
-        ),
-        html.Br(),
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
 
-        dbc.Row(
-            [
-                
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.Iframe(
-                                    id="lineplot",
-                                    style={"border-width": "0", "width": "100%", "height": "450px"}
-                                ),
-                                dcc.Dropdown(
-                                    id='xcol-lineplot-widget',
-                                    value='Total score',
-                                    options=[{'label': col, 'value': col} for col in ["Total score"]]
-                                )
-                            ]
-                        )
-                    ],
-                    width=9
-                ),
-                dbc.Col(
-                    [
-                   
-                    ],
-                    width=3
-                ),
-            ],
-        ),
-        html.Br(),
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
 
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        
-                    ],
-                ),
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.Iframe(
-                                    id="vertical_barplot",
-                                    # value="Total score",
-                                    style={"border-width": "0", "width": "100%", "height": "450px"}
-                                ),
-                                dcc.Dropdown(
-                                    id='xcol-vbarplot-widget',
-                                    value='Total score',
-                                    options=[{'label': col, 'value': col} for col in ["Total score"]]
-                                )
-                            ]
-                        )
-                    ]
-                ),
-            ],
-        )
-    ]
-)
-
-@app.callback(
-    Output("lineplot", "srcDoc"),
-    Output("vertical_barplot", "srcDoc"),
-    Input('xcol-lineplot-widget', 'value'),
-    Input('xcol-vbarplot-widget', 'value')
-)
-def vertical_barplot(xcol_lineplot, xcol_vbarplot):
-    lineplot = alt.Chart(qwl_df).mark_line().encode(
-        x=xcol_lineplot,
-        y="count()",
-    ).properties(width=600).interactive()
-
-    vertical_barplot = alt.Chart(qwl_df).mark_bar().encode(
-        x=alt.X(
-            xcol_vbarplot,
-            bin=alt.Bin(maxbins=5)
-        ),
-        y="count()",
-    )
-
-    return lineplot.to_html(), vertical_barplot.to_html(), 
-
-if __name__ == "__main__":
-    app.run_server(debug=False)
+if __name__ == '__main__':
+    app.run_server(debug=True)
